4cd9079c5f23ad1ce71bf3563b0c55bfa9141121
fmoges1 <fmoges@gmail.com>
2025-04-12 20:40:17 -0400

    Update app.py

--- app.py
+++ app.py
@@ -1,3 +1,11 @@
 import streamlit as st
 import pandas as pd
 import plotly_express as px
+
+df = pd.read_csv('vehicles_us.csv')
+df['manufacturer'] = df['model'].apply(lambda x:x.split()[0])
+
+# create a text header above the dataframe
+st.header('Data viewer') 
+# display the dataframe with streamlit
+st.dataframe(df)
